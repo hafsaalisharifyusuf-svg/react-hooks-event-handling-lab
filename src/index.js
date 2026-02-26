c7dff42f3eced31692619a6f1323beb02a9b7fda
hafsa <hafsaalisharifyusuf@gmail.com>
2026-02-25 22:39:43 -0800

    done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Keypad from './components/Keypad';
-import EyesOnMe from './components/EyesOnMe';
+import React from "react";
+import ReactDOM from "react-dom";
+import Keypad from "./components/Keypad";
+import EyesOnMe from "./components/EyesOnMe";
 
 ReactDOM.render(
-  <div>
+  <div style={{ padding: "20px" }}>
+    <h1>Event Handling Lab</h1>
+
+    <h2>Keypad</h2>
     <Keypad />
+
+    <h2>Eyes on Me</h2>
     <EyesOnMe />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
